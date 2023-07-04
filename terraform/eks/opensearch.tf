68e1237fabaf115908f3f0b371a541b1e066b019
Rohit kumar <rohit.kumar@tide.co>
2023-07-04 20:00:50 +0530

    fix os policy

--- terraform/eks/opensearch.tf
+++ terraform/eks/opensearch.tf
@@ -4,7 +4,7 @@ module "opensearch" {
   engine_version                                 = "OpenSearch_1.3"
   region                                         = var.region
   advanced_security_options_enabled              = true
-  default_policy_for_fine_grained_access_control = false
+  default_policy_for_fine_grained_access_control = true
   internal_user_database_enabled                 = true
   node_to_node_encryption                        = true
   encrypt_at_rest = {
